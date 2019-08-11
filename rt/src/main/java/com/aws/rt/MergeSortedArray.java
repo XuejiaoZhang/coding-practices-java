7e17629eab27c1a325dfb514335920bcf2688031
rogary+1-at-047651235753 <test-z@example.com>
2019-08-12 00:37:20 +0800

    0811 coding

---
+++ rt/src/main/java/com/aws/rt/MergeSortedArray.java
@@ -0,0 +1,28 @@
+package com.aws.rt;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int newLen = m + n -1;
+        int j = n - 1;
+        int i = m - 1;
+        for (int k = newLen; k >= 0; k--){
+            while (j >= 0 && i >= 0){
+                if (nums2[j] > nums1[i]) {
+                    nums1[k] = nums2[j];
+                    j -= 1;
+                    k -= 1;
+                } else {
+                    nums1[k] = nums1[i];
+                    i -= 1;
+                    k -= 1;
+                }
+            }
+        }
+
+        while (j >= 0){
+            nums1[j] = nums2[j];
+            j -= 1;
+        }
+
+    }
+}
